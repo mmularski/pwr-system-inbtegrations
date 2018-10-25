b9cc782e0d1b5cb7fa91e47bbb40f6ffef7a1fe2
Lukasz <lukasz.marszalek@interia.pl>
2018-10-25 17:34:44 +0000

    Init module

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+
+gem 'rubocop', require: false
+
+group :development do
+  gem "capistrano", "~> 3.8"
+  gem 'capistrano-magento2'
+end
---
+++ app/code/Newsletter/Saver/Block/Saver.php
@@ -0,0 +1,10 @@
+<?php
+namespace Newsletter\Saver\Block;
+
+class Saver extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'Hello world!';
+    }
+}
